8e2ffd02a18681c8bef16ab48a50780e36acc2e7
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-06-06 09:51:57 +0200

    Initial commit

---
+++ test/LedgerClient.spec.ts
@@ -0,0 +1,10 @@
+import * as supertest from 'supertest'
+import * as LedgerClient from '../src/LedgerClient'
+const config = require('../resources/config-fabric-network.json');
+
+describe('EventGeneratorInput', () => {
+  // it('works', () =>
+  // //
+  // //LedgerClient.ledgerClient.doQuery()
+  // )
+})
