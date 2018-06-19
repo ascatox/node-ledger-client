2a974ee37cf7f73d1e260b66a7693e2db8b0267c
Antonio Scatoloni <antonio.scatoloni@gmail.com>
2018-06-19 09:11:42 +0200

    Logger to info

--- src/Logger.ts
+++ src/Logger.ts
@@ -4,7 +4,7 @@ import * as moment from 'moment'
 const tsFormat = () => moment().format('DD-MM-YYYY HH:mm:ss').trim();
 
 const logger = new (winston.Logger)({
-    level: process.env.LOGGING_LEVEL || 'debug',
+    level: process.env.LOGGING_LEVEL || 'info',
     transports: [
         // colorize the output to the console
         new (winston.transports.Console)({
